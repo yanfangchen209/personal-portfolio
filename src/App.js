378772086285d53c53c76d13fe99f9019c6cfb25
yanfang chen <juliechen209@gmail.com>
2023-07-28 13:31:26 -0500

    create personal portfolio project using create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Welcome to my personal portfolio.
+    </div>
+ 
+ 
+  );
+}
+
+export default App;
