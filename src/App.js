ef83e5a35bef29f23346c0f3750d992677a0bcf1
yanfang chen <juliechen209@gmail.com>
2023-07-28 15:52:29 -0500

    add support page route using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      Welcome to my personal portfolio.
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/projects' element={<Projects/>} />
+          <Route path='/about' element={<About/>} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
+
  
  
   );
---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function About(){
+    return <div>about</div>;
+}
+
+export default About;
