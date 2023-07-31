56f53ac6c500bc31310ed2af4ad0dccaa41ab5cb
yanfang chen <juliechen209@gmail.com>
2023-07-30 23:23:28 -0500

    implement home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import "../styles/Home.css";
+import LinkedIn from "@mui/icons-material/LinkedIn";
+import GitHub from "@mui/icons-material/GitHub";
 
 
 function Home(){
-    return <div>Home</div>;
+    return (
+        <div className='home'>
+            <div className='about'>
+                <h2 className='intro'>Hello, I am Yanfang Chen.</h2>
+                <div className='desc'>
+                    <p>A new computer science graudate with passion for learning, currently looking for a job.</p>
+                    <a href='https://www.linkedin.com/in/yanfang--chen/' rel='noreferrer' target='_blank'><LinkedIn/></a>
+                    <a href='https://github.com/yanfangchen209' rel='noreferrer' target='_blank'><GitHub/></a>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Home;
